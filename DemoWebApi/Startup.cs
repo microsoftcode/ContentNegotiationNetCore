795b81c27011a129ee3b0a9b8831ec4a51586497
apatil <amit_patil@persistent.co.in>
2018-07-18 15:17:49 +0530

    Added Code for Swagger

--- DemoWebApi/Startup.cs
+++ DemoWebApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DemoWebApi
 {
@@ -24,25 +25,51 @@ namespace DemoWebApi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc() // This is default line of code 
-				.AddXmlSerializerFormatters() //Added this line to return if Request is made for XML 
-				.AddMvcOptions(setUpAction =>
-				{
-					setUpAction.OutputFormatters.Add(new CsvOutputFormatter());
-				}
-				);
-			    
+            services.AddMvc() // This is default line of code 
+                .AddXmlSerializerFormatters() //Added this line to return if Request is made for XML 
+                .AddMvcOptions(setUpAction =>
+                {
+                    setUpAction.OutputFormatters.Add(new CsvOutputFormatter());
+                }
+                );
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info { Title = "Demo API", Version = "v1" });               
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();          
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+           
+
+            // Enable middleware to serve swagger-ui
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
